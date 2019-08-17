fd23559eb97c0410d417b96e47ee94a8b3804be3
Dwi <ruckuus@gmail.com>
2019-08-17 18:54:09 +0800

    Building list meal and meal item.

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app_v2/widgets/meal_item.dart';
+
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          var meal = categoryMeals[index];
+          return MealItem(
+            title: meal.title,
+            imageUrl: meal.imageUrl,
+            complexity: meal.complexity,
+            affordability: meal.affordability,
+            duration: meal.duration,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
